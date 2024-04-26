f23073932c423b0a79ceaa6b2d48452764192c4c
cptestjohn <cptestjohn@gmail.com>
2024-04-26 07:29:01 +0530

    My first sum added.

---
+++ src/mypkg/MyCalc.java
@@ -0,0 +1,17 @@
+package mypkg;
+
+public class MyCalc {
+
+	int sum(int a , int b)
+	{
+		return a+b;
+	}
+	
+	public static void main(String[] args) {
+		
+		MyCalc calc= new MyCalc();
+		System.out.println("Sum is "+calc.sum(20,10));
+		
+	}
+
+}
